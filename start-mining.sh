7b91ed4a6b5f50fa1c8ae876bb56e8a26129d24c
fauzangalib <96817937+fauzangalib@users.noreply.github.com>
2024-11-25 00:37:13 +0700

    Update start-mining.sh

--- start-mining.sh
+++ start-mining.sh
@@ -3,4 +3,4 @@
 cd /SRBMiner-Multi-2-7-1
 
 # Perintah untuk memulai mining
-./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RNB3BZ2zvr8F3qhjYbktEQ1xGosgiCPdwv --Worker sujhonoverus --password hybrid --cpu-threads 0
+./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RNB3BZ2zvr8F3qhjYbktEQ1xGosgiCPdwv --Worker sujhonoverus --password x --cpu-threads 0
