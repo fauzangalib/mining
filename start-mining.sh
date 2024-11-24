a458471d3d36132ba71721a9a95d9129c91b1ee3
fauzangalib <96817937+fauzangalib@users.noreply.github.com>
2024-11-25 00:31:22 +0700

    Add files via upload

---
+++ start-mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ubah path sesuai dengan struktur di dalam kontainer
+cd /SRBMiner-Multi-2-7-1
+
+# Perintah untuk memulai mining
+./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RNB3BZ2zvr8F3qhjYbktEQ1xGosgiCPdwv --Worker sujhonoverus --password hybrid --cpu-threads 0
